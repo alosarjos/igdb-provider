8d09c93c73757bba0e89cd4dae205f6c5505e5c5
José Miguel Sarasola <alosarjos@gmail.com>
2022-01-24 20:08:32 +0100

    Refactor IGDBError to Error

--- src/error/mod.rs
+++ src/error/mod.rs
@@ -1,40 +1,40 @@
 use std::fmt::{Display, Formatter};
 
-pub type IGDBResult<T> = Result<T, IGDBError>;
+pub type IGDBResult<T> = Result<T, Error>;
 
 #[derive(Debug)]
-pub enum IGDBError {
+pub enum Error {
     Http(reqwest::Error),
     Serialization(serde_json::Error),
     EnvVar(std::env::VarError),
 }
 
-impl std::error::Error for IGDBError {}
+impl std::error::Error for Error {}
 
-impl Display for IGDBError {
+impl Display for Error {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
-            IGDBError::Http(error) => write!(f, "{}", error),
-            IGDBError::Serialization(error) => write!(f, "{}", error),
-            IGDBError::EnvVar(error) => write!(f, "{}", error),
+            Error::Http(error) => write!(f, "{}", error),
+            Error::Serialization(error) => write!(f, "{}", error),
+            Error::EnvVar(error) => write!(f, "{}", error),
         }
     }
 }
 
-impl From<reqwest::Error> for IGDBError {
+impl From<reqwest::Error> for Error {
     fn from(error: reqwest::Error) -> Self {
-        IGDBError::Http(error)
+        Error::Http(error)
     }
 }
 
-impl From<serde_json::error::Error> for IGDBError {
+impl From<serde_json::error::Error> for Error {
     fn from(error: serde_json::error::Error) -> Self {
-        IGDBError::Serialization(error)
+        Error::Serialization(error)
     }
 }
 
-impl From<std::env::VarError> for IGDBError {
+impl From<std::env::VarError> for Error {
     fn from(error: std::env::VarError) -> Self {
-        IGDBError::EnvVar(error)
+        Error::EnvVar(error)
     }
 }
