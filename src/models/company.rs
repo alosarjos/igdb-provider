94177c057b634b7ff023a0d9a4fc9f7cf18527c1
José Miguel Sarasola <alosarjos@gmail.com>
2022-04-17 17:34:04 +0200

    Impl: Add clone derive to Company

--- src/models/company.rs
+++ src/models/company.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct Company {
     pub id: i32,
     pub name: String,
