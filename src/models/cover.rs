76a4fbf91375a33cc8d2799225df3f6e206cdf41
José Miguel Sarasola <alosarjos@gmail.com>
2022-04-17 17:35:01 +0200

    Set the correct URL when deserializing screenshots

--- src/models/cover.rs
+++ src/models/cover.rs
@@ -1,19 +1,19 @@
 use serde::{Deserialize, Deserializer, Serialize};
 
 #[derive(Debug, Deserialize, Serialize)]
-pub struct Screenshot {
+pub struct Cover {
     pub id: i32,
-    #[serde(deserialize_with = "screenshot_url_mapper")]
+    #[serde(deserialize_with = "cover_url_mapper")]
     pub url: String,
 }
 
-pub fn screenshot_url_mapper<'de, D>(deserializer: D) -> Result<String, D::Error>
+pub fn cover_url_mapper<'de, D>(deserializer: D) -> Result<String, D::Error>
 where
     D: Deserializer<'de>,
 {
     let value = String::deserialize(deserializer)?;
     Ok(format!(
         "https:{}",
-        value.replace("t_thumb", "t_screenshot_huge"),
+        value.replace("t_thumb", "t_cover_big_2x"),
     ))
 }
